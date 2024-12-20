6668a78a0e0053eb22100144bf75f35821a6c86c
sadeq <sadeghtpf13@gmail.com>
2024-12-21 00:14:47 +0330

    First  commit
    just set stracture that what i want to do

---
+++ Product_recomendation.py
@@ -0,0 +1,21 @@
+import json
+
+
+def read_json(filename):
+    try:
+        with open(filename) as file:
+            for product in file:
+                pass
+    except:
+        pass
+
+
+def binary_search(data, target):
+    try:
+        pass
+    except:
+        pass
+
+
+if __name__ == '__main__':
+    read_json('product.json')
