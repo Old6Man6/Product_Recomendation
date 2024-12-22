24d03703cd3b52aa739a615b03075607c7cd7feb
sadeq <sadeghtpf13@gmail.com>
2024-12-22 23:27:28 +0330

    2
    class Product:
    create Product class and in read_json function read the json file and make an instance of Product class for each product in list.
    
    read_json(): -->
    first condition for beggining binary search is that data must be sorted.

--- Product_recomendation.py
+++ Product_recomendation.py
@@ -1,21 +1,39 @@
 import json
 
+class Product:
+    def __init__(self, name, price, rating):
+        self.name = name
+        self.price = price
+        self.rating = rating
+
 
 def read_json(filename):
+    products = []
     try:
         with open(filename) as file:
-            for product in file:
-                pass
+            for p in json.load(file):
+                products.append(Product(p['name'], p['price'], p['rating']))
+            return products
     except:
         pass
+
 
 
 def binary_search(data, target):
     try:
-        pass
+        data.sort(key=lambda x: x.price)
+        for product in data:
+            print(product.price)
     except:
         pass
 
 
+
+
+
+
+
 if __name__ == '__main__':
-    read_json('product.json')
+    target = input("Enter a Price: ")
+    data = read_json('products.json')
+    binary_search(data=data, target=target)
